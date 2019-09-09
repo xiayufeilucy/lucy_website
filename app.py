32cfab0c79ba1214ad975e8c002c3393f7edc37c
Yufei Xia <yufeix@wharton.upenn.edu>
2019-09-09 00:24:45 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.secret_key = "lucy"
+Bootstrap(app)
+
+@app.route('/', methods= ["GET", "POST"])
+def search():
+    return render_template('index.html')
+
+@app.route('/about')
+def start_dashboard():
+    return render_template('about.html')
+
+@app.route('/works')
+def start_dashboard_2():
+    return render_template('works.html')
+
+
+if __name__ == '__main__':
+    app.run()
